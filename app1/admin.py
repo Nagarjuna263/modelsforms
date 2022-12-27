b1abd9479b7747fdf4e579605a3010d114f7d395
Nagarjuna <nagarjunareddy263@gmail.com>
2022-12-27 16:44:23 +0530

    first commit

--- app1/admin.py
+++ app1/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
 from app1.models import *
-
+# Register your models here.
+class WebpageCustomView(admin.ModelAdmin):
+    list_display=['topic_name','name','url']
+    list_editable=['name']
+    list_display_links=['topic_name','url']
+    #list_per_page=2
+    search_fields=['name']
+    list_filter=['name','topic_name']
+    
+class AccessRecordsCustomView(admin.ModelAdmin):
+    list_display=['date','name']
+    list_editable=['name']
+    #list_per_page=2
+    
+    
+    
+    
 admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(AccessRecords)
+admin.site.register(Webpage,WebpageCustomView)
+admin.site.register(AccessRecords,AccessRecordsCustomView)
--- app1/forms.py
+++ app1/forms.py
@@ -9,9 +9,16 @@ class TopicForm(forms.ModelForm):
 class WebpageForm(forms.ModelForm):
     class Meta:
         model=Webpage
-        fields="__all__"
+        #fields="__all__"
+        fields=['topic_name','name']
+        #exclude=['name']
+        help_texts={'topic_name':'shouldnot integers','name': 'only integers'}
+        labels={'topic_name':'TN','name':'N'}
+        widgets={'url':forms.PasswordInput,'name':forms.Textarea}
         
 class AccessRecordsForm(forms.ModelForm):
     class Meta:
         model=AccessRecords
         fields="__all__"
+        #fields=['date']
+        #exclude=['name']
